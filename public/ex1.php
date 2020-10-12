65b56f24294c34c15796c4e9197ada6e3aeffa1b
tireland1985 <56227226+tireland1985@users.noreply.github.com>
2020-10-12 11:54:07 +0100

    task 4

--- public/ex1.php
+++ public/ex1.php
@@ -1,13 +1,22 @@
 <?php
 $number = rand(1, 6);
+$number2 = rand(1, 6);
 
-if ($number == 6) {
-    echo 'You rolled a 6, you win!';
+if ($number == 6 && $number2 == 6) {
+    echo 'You rolled a ';
+    echo $number;
+    echo ' and a ';
+    echo $number2;
+    echo ', you win!';
 }
+
 else {
     echo 'You rolled a ';
     echo $number;
-    echo ', you lose.';
+    echo ' and a ';
+    echo $number2;
+    echo ', you lose';
 }
+
 echo '<a href="ex1.php"> Roll again</a>';
 ?>
